ce3e3c88e3d069f0bc0c17a4d940a0397670884d
coolcool <coolcool.with.you@gmail.com>
2024-04-14 18:26:52 +0900

    ADD - create mz-be project

---
+++ src/main/kotlin/coolcool/mz/MzApplication.kt
@@ -0,0 +1,11 @@
+package coolcool.mz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MzApplication
+
+fun main(args: Array<String>) {
+	runApplication<MzApplication>(*args)
+}
---
+++ src/test/kotlin/coolcool/mz/MzApplicationTests.kt
@@ -0,0 +1,13 @@
+package coolcool.mz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MzApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
