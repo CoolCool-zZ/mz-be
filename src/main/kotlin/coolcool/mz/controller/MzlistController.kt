8d84c8af289cbc0a8b5d5f87bcaa15d49d6f917e
coolcool <coolcool.with.you@gmail.com>
2024-04-19 22:13:06 +0900

    ADD - controller에 swagger 관련 설명 추가

--- src/main/kotlin/coolcool/mz/controller/MzlistController.kt
+++ src/main/kotlin/coolcool/mz/controller/MzlistController.kt
@@ -4,6 +4,8 @@ import coolcool.mz.dto.ResponseDto
 import coolcool.mz.dto.mzlist.MzlistDetailReq
 import coolcool.mz.dto.mzlist.MzlistDetailRes
 import coolcool.mz.service.MzlistService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Positive
 import org.springframework.http.MediaType
@@ -16,11 +18,14 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "mzlist")
 @Validated
 @RestController
 class MzlistController(
     private val mzlistService: MzlistService,
 ) {
+
+    @Operation(description = "create mzlist")
     @PostMapping("/mzlist", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun createMzlist(
         @Valid @RequestBody mzlistDetailReq: MzlistDetailReq,
@@ -28,6 +33,7 @@ class MzlistController(
         return ResponseDto.ofSuccess(mzlistService.createMzlist(mzlistDetailReq))
     }
 
+    @Operation(description = "update mzlist")
     @PutMapping("/mzlist/{mzlistId}", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun updateMzlist(
         @PathVariable("mzlistId") @Positive(message = "mzlistId should be positive") mzlistId: Long,
@@ -40,6 +46,7 @@ class MzlistController(
         return ResponseDto.ofSuccess(mzlistService.updateMzlist(mzlistId, mzlistDetailReq))
     }
 
+    @Operation(description = "delete mzlist")
     @DeleteMapping("/mzlist/{mzlistId}")
     fun deleteMzlist(
         @PathVariable("mzlistId") @Positive(message = "mzlistId should be positive") mzlistId: Long,
@@ -49,11 +56,13 @@ class MzlistController(
         return ResponseDto.ofSuccessWithoutData()
     }
 
+    @Operation(description = "get user owned mzlist detail")
     @GetMapping("/mzlist/owned")
     fun getMyOwnedMzlistList(): ResponseDto<List<MzlistDetailRes>> {
         return ResponseDto.ofSuccess(mzlistService.getMyOwnedMzlistList())
     }
 
+    @Operation(description = "get user owned mzlist list")
     @GetMapping("/mzlist/owned/{mzlistId}")
     fun getMyOwnedMzlistDetail(
         @PathVariable("mzlistId") @Positive(message = "mzlistId should be positive") mzlistId: Long,
--- src/main/kotlin/coolcool/mz/controller/UserController.kt
+++ src/main/kotlin/coolcool/mz/controller/UserController.kt
@@ -3,17 +3,21 @@ package coolcool.mz.controller
 import coolcool.mz.dto.ResponseDto
 import coolcool.mz.dto.user.UserDetailRes
 import coolcool.mz.service.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.constraints.Positive
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "user")
 @Validated
 @RestController
 class UserController(
     private val userService: UserService,
 ) {
+    @Operation(description = "get user detail")
     @GetMapping("/user/{userId}")
     fun getUserDetail(
         @PathVariable(name = "userId") @Positive(message = "userId should be positive") userId: Long,
